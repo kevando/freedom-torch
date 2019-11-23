b28dadb9ef9ae52aa17d22ec486a0a1bff64b7ff
kevando <khabich@gmail.com>
2019-11-22 17:32:00 -0800

    wip

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div>
-      <ul className="nav">
+    <div className="home">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Congress Connections</title>
+      </Helmet>
+      <h1 className="logo">congress connections</h1>
+      <h5 className="center">Exploring how congress uses Twitter</h5>
+
+      <div className="cards">
+
+        <div className="card senate-map">
+        <Link to="/senate-map">Senate Map</Link>
+
+        
+        </div>
+
+        <div className="card senate-table">
+        <Link to="/senate-table">Senate Table</Link>
+        </div>
+
+
+        </div>
+      {/* <ul className="nav">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
           <Link to="/senate-map">Senate Map</Link>
-        </li>
-      </ul>
+        </li> */}
+
     </div>
   );
 };
